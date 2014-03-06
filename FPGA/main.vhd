d64abd0c83e16e6534392051e21e62311db11632
Anders Bæk <anbae12@student.sdu.dk>
2014-03-06 14:14:21 +0100

    FPGA base lavet. Jeg oprettede projekt med .ucf. Vi kan lige tage en snak omkring det på tirsdag.

---
+++ FPGA/main.vhd
@@ -0,0 +1,30 @@
+----------------------------------------------------------------------------------
+-- Company:  Gruppe 2 - Semesterprojekt 2014
+
+-- Module Name:    		main - Behavioral
+-- Module description:  
+
+-- Change date		Engineer 
+-- 06-03-2014 		ALB
+--
+--
+--
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+----------------------------------------------------------------------------------
+
+entity main is
+end main;
+
+architecture Behavioral of main is
+
+begin
+
+
+end Behavioral;
+
